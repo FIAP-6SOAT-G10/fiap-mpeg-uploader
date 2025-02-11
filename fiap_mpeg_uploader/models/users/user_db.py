a559c52fe0bdcc957da164b81413613b0c8d2044
gabriwl165 <gabrielmarcelino165@gmail.com>
2025-02-11 18:57:20 -0300

    fix: sonar pipeline

--- fiap_mpeg_uploader/models/users/user_db.py
+++ fiap_mpeg_uploader/models/users/user_db.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, field_validator, Field, AliasChoices
+from typing import Any
+from pydantic_mongo import ObjectIdField
 
 class UserDb(BaseModel):
-    id: str = Field(alias="id")
+    id: str | ObjectIdField = Field(alias="id", validation_alias=AliasChoices("id", "_id"))
     login: str
     password: str
     name: str
     email: str
+
+    @field_validator("id")
+    @classmethod
+    def convert_object_id_into_str(cls, data: Any) -> Any:  
+        if isinstance(data, ObjectIdField):  
+            return str(data)
+        return data
